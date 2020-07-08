a3e0bc65a1a27dfaf5e197adf9f5a415e42cfe0d
Yaroslaw Korol <y.korol93@gmail.com>
2020-07-08 11:40:06 +0300

    Added Secret

---
+++ Secret.py
@@ -0,0 +1,10 @@
+while True:
+ name=input("Hi,Whats your name?")
+ Johny = "Johny"
+ johny = "johny" 
+ Johny=johny
+ if str(name) == str(johny):
+    print("Hi", str(name), "You look great")
+ else:
+    print("Hi",str(name))
+ 
